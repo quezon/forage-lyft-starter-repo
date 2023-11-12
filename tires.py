3f9291ae9f0ab2905fce88d6d12b4b2762176ebf
quezon <gregoryallegory@gmail.com>
2023-11-11 22:37:18 -0500

    change to sum of list

--- tires.py
+++ tires.py
@@ -10,15 +10,13 @@ class CarriganTires(Tires):
     def __init__(self):
         self.worn_atleast_one = 0.9
     def tires_should_be_serviced(self):
-        for tirewear in self.tires:
-            if tirewear >= self.worn_atleast_one:
+        for tire_wear in self.tires:
+            if tire_wear >= self.worn_atleast_one:
                 return True
+        return False
 
 class OctoprimeTires(Tires):
     def __init__(self):
         self.worn_all = 3
     def tires_should_be_serviced(self):
-        sumwear = 0
-        for tirewear in self.tires:
-            sumwear += tirewear
-        return sumwear >= self.worn_all
+        return sum(self.tires) >= self.worn_all
